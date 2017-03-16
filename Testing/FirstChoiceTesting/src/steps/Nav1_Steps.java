10d6edc6fed44ba74bdbecc18d5f49adcfeb86c4
mrsmaiden2b <mrsmaiden2b@hotmail.co.uk>
2017-03-16 13:53:33 +0000

    Updated Testing

--- Testing/FirstChoiceTesting/src/steps/Nav1_Steps.java
+++ Testing/FirstChoiceTesting/src/steps/Nav1_Steps.java
@@ -1,7 +1,9 @@
 package steps;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -15,26 +17,29 @@ public class Nav1_Steps
 	@Before
 	public void setUp()
 	{
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Vanilla/workspaceRC/FirstChoiceTesting/src/chromedriver.exe");
+		driver = new ChromeDriver();
 	}
 	
 	@Given("^user navigates to website home page$")
 	public void user_navigates_to_website_home_page() throws Throwable 
 	{
-		driver.get("https://localhost/project-one-website/index");
+		driver.get("https://localhost/project-one-website/index.html");
 		driver.manage().window().maximize(); 
 	}
 
 	@When("^user selects Apprenticeships Infrastructure$")
 	public void user_selects_Apprenticeships_Infrastructure() throws Throwable 
 	{
-	  
+		//driver.WaitForPageToLoad( "30000" );
+		driver.findElement(By.id("toggler")).click();
+		driver.findElement(By.id("toggler")).click();
 	}
 
 	@Then("^user is navigated to Appenticeships Infratructure page$")
 	public void user_is_navigated_to_Appenticeships_Infratructure_page() throws Throwable 
 	{
-	    
+		driver.findElement(By.linkText("IT Infrastructure")).click();
 	}
 
 	@Then("^user selects to Apprenticeships Creative Media$")
