cad683cdaefe7e3837c155f3bd1325d28845cb6e
Kieran M <kieranmcbride13@outlook.com>
2017-03-16 11:09:22 +0000

    More Changes

--- index.js
+++ index.js
@@ -2,45 +2,41 @@ const http = require('http');
 const fs = require('fs');
 const formidable = require('formidable');
 const util = require('util');
+const port = 1337;
 
-
-
+//Creating the node.js webserver.
 var server = http.createServer((req, res) => {
-    if(req.method.toLowerCase() === 'get') {
+    //Anonymous func to route by request type.
+    if (req.method.toLowerCase() === 'get') {
         present(res);
-    } else if(req.method.toLowerCase() === 'post') {
+    } else if (req.method.toLowerCase() === 'post') {
         processForm(req, res);
     }
-});
+}).listen(port);
 
+//Presents form.
 function present(res) {
     fs.readFile('Contact.html', (err, data) => {
         res.writeHead(200, {
             'Content-Type': 'text/html',
-                'Content-Length': data.length
+            'Content-Length': data.length
         });
         res.write(data);
         res.end();
     })
 }
 
+//Presents the data from the POST request.
 function processForm(req, res) {
     var form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, fields) => {
         res.writeHead(200, {
             'content-type': 'text/plain'
         });
         res.write('Data Received:\n\n');
         res.end(util.inspect({
             fields: fields,
-            files, files
         }));
     });
-} 
-
-
-
-
-
-server.listen(1185);
-console.log("Listening on : 1185");
+}
+console.log("Listening on : " + port);
