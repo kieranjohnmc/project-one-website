3794745cc6be306bb19b8a66ce3bd1133be93e31
fuzew <kieranmcbride13@outlook.com>
2017-03-16 22:01:48 +0000

    Node final push

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const http = require('http');
 const fs = require('fs');
 const formidable = require('formidable');
 const util = require('util');
-const port = 1337;
+const mysql = require('mysql');
 
-var cssHeaders = 'text/css';
-var jsHeaders = 'text/javascript';
+const port = 1337;
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ale1'
+});
+
+var queryData = [];
+
+
+conn.connect((err) => {
+    var sql = 'SELECT * FROM messages';
+
+    conn.query(sql, (err, rows, f) => {
+        if (err) {
+            console.log(err);
+        }
+        connection.end();
+
+        for (x = 0; x < rows.length; x++) {
+            queryData[i] = rows;
+        }
+    });
+});
 
 //Creating the node.js webserver.
 var server = http.createServer((req, res) => {
@@ -32,13 +55,14 @@ function present(res) {
 //Presents the data from the POST request.
 function processForm(req, res) {
     var form = new formidable.IncomingForm();
-    form.parse(req, (err, fields) => {
+    form.parse(req, (err, fields, files) => {
         res.writeHead(200, {
             'content-type': 'text/plain'
         });
         res.write('Data Received:\n\n');
         res.end(util.inspect({
             fields: fields,
+            files: files
         }));
     });
 }
